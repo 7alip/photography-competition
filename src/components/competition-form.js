8f21cf193a4f18548c43723703f473a8c7358aad
7alip <talipaltas@gmail.com>
2020-12-15 23:24:10 +0100

    fix: photo upload

--- src/components/competition-form.js
+++ src/components/competition-form.js
@@ -73,14 +73,12 @@ const CompetitionForm = () => {
     title: yup.string().required(t('form.errors.title_required')),
     story: yup.string(),
     accepted: yup.bool().oneOf([true], t('form.errors.accept_required')),
-    image: yup
-      .mixed()
-      .required(t('form.errors.photo_required'))
-      .test(
-        'fileSize',
-        t('form.errors.photo_size'),
-        value => value && value[0] && value[0].size <= 5 * 1000 * 1000
-      ),
+    image: yup.mixed().required(t('form.errors.photo_required')),
+    // .test(
+    //   'fileSize',
+    //   t('form.errors.photo_size'),
+    //   value => value && value[0] && value[0].size <= 15 * 1000 * 1000
+    // ),
   })
 
   const { register, handleSubmit, errors } = useForm({
